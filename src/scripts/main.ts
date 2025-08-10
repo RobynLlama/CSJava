5a8c37daa4a5b99aeddf216cabca5a89c4d5bbcf
Robyn <rnixon98@gmail.com>
2025-08-09 19:42:13 -0500

    feat(scripts): Callback example using network

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,10 @@
 import { greetings } from "./module.js";
 
+function butts(message: string)
+{
+  Wrapper.log(`Butts was called ${message}`);
+}
+
 Wrapper.log("Greetings from main");
+Wrapper.network.openNetworkPort(100, butts);
 greetings();
--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,4 +1,5 @@
 export function greetings()
 {
   Wrapper.log("hello from another file");
+  Wrapper.network.writeToNetworkPort(100, "Hello from greetings!");
 }
